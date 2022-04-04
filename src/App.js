fcf9feef13640e9a7d0e323f666728c675049b0c
ikenwaa <ajohn9353@gmail.com>
2022-04-04 15:02:34 +0100

    removed comments from App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com/?apikey=cecaf37d'
 
-// const movie = {
-//     "Title": "Amazing Spiderman Syndrome",
-//     "Year": "2012",
-//     "imdbID": "tt2586634",
-//     "Type": "movie",
-//     "Poster": "N/A"
-// };
-
 const App = () => {
 
     const [movies, setMovies] = useState([]);
